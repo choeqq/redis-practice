77d2d4c605019f8b0a3fb99222a4010ae7c9c023
choeqq <k.chola@op.pl>
2022-05-29 23:18:41 +0200

    300ms -> 20ms improvement

--- server.js
+++ server.js
@@ -3,18 +3,29 @@ const axios = require("axios");
 const cors = require("cors");
 const Redis = require("redis");
 
+const redisClient = Redis.createClient();
+
+const DEFAULT_EXPIRATION = 3600;
+
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.get("/photos", async (req, res) => {
   const albumId = req.query.albumId;
-  const { data } = await axios.get(
-    "https://jsonplaceholder.typicode.com/photos",
-    { params: { albumId } }
-  );
-
-  res.json(data);
+  redisClient.get("photos", async (error, photos) => {
+    if (error) console.error(error);
+    if (photos) {
+      return res.json(JSON.parse(photos));
+    } else {
+      const { data } = await axios.get(
+        "https://jsonplaceholder.typicode.com/photos",
+        { params: { albumId } }
+      );
+      redisClient.setex("photos", DEFAULT_EXPIRATION, JSON.stringify(data));
+    }
+    res.json(data);
+  });
 });
 
 app.get("/photos/:id", async (req, res) => {
